9ae74073b383c37aff5151a886c18875ba50b5c9
noureddein <noureddein@gmail.com>
2021-01-27 16:26:21 +0200

    withJava

--- java-dynamic.js
+++ java-dynamic.js
@@ -3,11 +3,21 @@ function cdNumber(){
     var userNumber = prompt("Pleas type you number?!");
     if (userNumber > 4 && userNumber < 8){
         document.getElementById("results").innerHTML = ((0.15 * 1.99)*userNumber);
+        document.getElementById("results2").innerHTML=("Your Discount is 15%");
     }else if (userNumber >= 8 && userNumber <10 ) {
         document.getElementById("results").innerHTML = ((0.20 * 1.99)*userNumber);
-    }else (userNumber >10 ) ;{
+        document.getElementById("results2").innerHTML=("Your Discount is 20%");
+    }else if (userNumber >10 ) {
         document.getElementById("results").innerHTML = ((0.35 * 1.99)*userNumber);
-        document.getElementById("results2").innerHTML=("You Got 2 CDs FREE! :)");
+        document.getElementById("results2").innerHTML=("You Got 2 CDs FREE! :) and Your Discount is 35%");
+
+    }
+    else if (userNumber == 10 ) {
+        document.getElementById("results").innerHTML = ((0.35 * 1.99)*userNumber);
+        document.getElementById("results2").innerHTML=("Your Discount is 30%");
+    }
+    else{
+        confirm("Sorry you will NOT get any Discount!! :(");
     }
 
 }
